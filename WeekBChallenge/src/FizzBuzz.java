4f4d368c94604104cc3cab44b47350e947fff749
william oleson <wnoleson@gmail.com>
2019-03-25 19:53:11 -0400

    reread/examined fizzBuzz4 for clarity and peace of mind.  attempt 2

--- WeekBChallenge/src/FizzBuzz.java
+++ WeekBChallenge/src/FizzBuzz.java
@@ -1,17 +1,17 @@
 
 public class FizzBuzz {
-	
+
 	public static void fizzBuzz1(int i){
-	
-		
+
+
 		if(i % 15 == 0) {
 			System.out.println("FizzBuzz");
 		}else if(i % 5 == 0) {
 			System.out.println("Buzz");
-			
+
 		}else if(i % 3 == 0) {
 			System.out.println("Fizz");
-		
+
 		}else {
 			for(int i1 = 0; i1 <=100; i1++) {
 				System.out.println(i1);
@@ -20,20 +20,20 @@ public class FizzBuzz {
 	}
 
 	public static void fizzBuzz2(int m,int n){
-		
+
 		for(int i = m; i <=n; i++) {
 			System.out.println(i);
-	     
+
 			if(i % 15 == 0) {
 				System.out.println("FizzBuzz");
 			}else if(i % 5 == 0) {
 				System.out.println("Buzz");
-				
+
 			}else if(i % 3 == 0) {
 				System.out.println("Fizz");
-		
+
 			}
-		
+
 	     }
 
 	}
@@ -42,7 +42,7 @@ public class FizzBuzz {
 		int z = fizzNum + buzzNum;
 		for(int i = m; i <=n; i++) {
 			System.out.println(i);
-			
+
 			if(i % fizzNum == 0 && i % buzzNum == 0) {
 				System.out.println("FizzBuzz");
 			}else if(i % buzzNum == 0) {
@@ -51,23 +51,37 @@ public class FizzBuzz {
 				System.out.println("Fizz");
 			}
 		}
-		
+
 	}
 
-	public static void fizzBuzz4(int m, int n, int[] numbers, String[] terms){
-		for(int i = m; i <=n; i++) {
-			System.out.println(i);
-			
-			if(i % numbers[0] == 0 && i % numbers[1] == 0) {
-				System.out.println(terms[2]);
-			}else if(i % numbers[1] == 0) {
-				System.out.println(terms[1]);
-			}else if(i % numbers[0] == 0) {
-				System.out.println(terms[0]);
-			}
-		 }
-	
-	 }	
-	
+	// public static void fizzBuzz4(int m, int n, int[] numbers, String[] terms){
+	// 	for(int i = m; i <=n; i++) {
+	// 		System.out.println(i);
+
+	// 		if(i % numbers[0] == 0 && i % numbers[1] == 0) {
+	// 			System.out.println(terms[2]);
+	// 		}else if(i % numbers[1] == 0) {
+	// 			System.out.println(terms[1]);
+	// 		}else if(i % numbers[0] == 0) {
+	// 			System.out.println(terms[0]);
+	// 		}
+	// 	 }
+
+	//  }
+
+  public static void fizzBuzz4(int m, int n, int[] numbers, String[] terms){
+    for(int i = m; i <=n; i++) {
+      System.out.println(i);
+
+      if(i % 2 == 0){
+        System.out.println(terms[1]);
+      }else{
+        System.out.println(terms[0]);
+      }
+
+     }
+
+   }
+
 }
 
--- WeekBChallenge/src/FizzTest.java
+++ WeekBChallenge/src/FizzTest.java
@@ -1,16 +1,16 @@
 
 public class FizzTest extends FizzBuzz {
-		
+
 	public static void main(String[] args) {
-//		FizzBuzz fb = new FizzBuzz();
-		FizzBuzz.fizzBuzz1(3);
-		FizzBuzz.fizzBuzz1(10);
-		FizzBuzz.fizzBuzz1(15);
-		FizzBuzz.fizzBuzz2(30, 50);
-		FizzBuzz.fizzBuzz3(20, 100, 2, 5);
-		String[] str = new String[]{"fizz", "buzz", "fizzbuzz"};
-		int[] nums = {6, 8};
-		FizzBuzz.fizzBuzz4(1, 24, nums, str);
-	}
+    // FizzBuzz fb = new FizzBuzz();
+    FizzBuzz.fizzBuzz1(3);
+    FizzBuzz.fizzBuzz1(10);
+    FizzBuzz.fizzBuzz1(15);
+    FizzBuzz.fizzBuzz2(30, 50);
+    FizzBuzz.fizzBuzz3(20, 100, 2, 5);
+    String[] str = new String[]{"fizz", "buzz"};
+    int[] nums = {1, 10};
+    FizzBuzz.fizzBuzz4(1, 100, nums, str);
+  }
 
 }
